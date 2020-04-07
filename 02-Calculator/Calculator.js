4f9aa971742b2bd2895ce3c845e21f8825bf7166
NadineDon <Nadine.Donie@stud.uni-regensburg.de>
2020-04-07 15:22:17 +0200

    Bearbeitung-Uebung2-ND

--- 02-Calculator/Calculator.js
+++ 02-Calculator/Calculator.js
@@ -3,14 +3,22 @@ import {MyMath} from "../01-MyMath/MyMath.js";
 export class Calculator {
 
     constructor(numpad, outputCalculation, outputSolution) {
+        this.numpad = numpad;
+        this.outputCalculation = outputCalculation;
+        this.outputSolution = outputSolution;
+        this.setupNumPad();        
     }
 
     setupNumPad() { 
+        var listofallnumbers = this.numpad.children;
+        for (let element of listofallnumbers) {
+            element.addEventListener('click', this.onButtonClick.bind(this, listofallnumbers))
+        }
 
     }
 
     onButtonClick(symbol) {
-    
+        console.log(symbol);
     }
 
     print(string) {
---
+++ 02-Calculator/index.js
@@ -0,0 +1,10 @@
+import {Calculator} from './Calculator.js'
+window.Calculator = Calculator;
+
+let numpad = document.querySelector(".numpad");
+let outputCalculation = document.querySelector("#calculation");
+let outputSolution = document.querySelector("#solution");
+
+window.calc = new Calculator(numpad, outputCalculation, outputSolution);
+
+
